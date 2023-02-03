0c9ffee60472e70928abf67357638dd4f5328fa7
Sergei Cherniaev <sergei@cherniaev.com>
2023-02-03 14:54:20 +0400

    feat(tests): add example interaction test with play function to Input component story

--- components/input/stories/Input.stories.tsx
+++ components/input/stories/Input.stories.tsx
@@ -1,8 +1,12 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
 import { Input } from "../Input";
 
 export default {
+  title: "Components/Input",
   component: Input,
   argTypes: {
     type: {
@@ -12,10 +16,27 @@ export default {
   },
 } as ComponentMeta<typeof Input>;
 
-const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+const Template: ComponentStory<typeof Input> = (args) => <Input data-testid="input" {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Empty = Template.bind({});
+Empty.args = {
   type: "text",
   disabled: false,
+};
+
+export const FilledInput = Template.bind({});
+Empty.args = {
+  type: "text",
+  disabled: false,
+};
+FilledInput.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // Simulate interactions with the component
+  await userEvent.type(canvas.getByTestId('input'), 'some value');
+
+  // Assert DOM structure
+  await expect(
+    canvas.getByTestId('input')
+  ).toBeInTheDocument();
 };
