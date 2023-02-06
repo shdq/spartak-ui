cb68688c9782e1ea5d372d0e3c95b63c317d292c
Sergei Cherniaev <sergei@cherniaev.com>
2023-02-06 13:06:32 +0300

    chore(deps): add config for stitches, add color tokens, update rollup config (#5)

---
+++ components/stitches.config.ts
@@ -0,0 +1,44 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled } = createStitches({
+  theme: {
+    colors: {
+      red000: "#FAE6E6",
+      red100: "#F6CCCC",
+      red200: "#EC9999",
+      red300: "#E88080",
+      red400: "#E36666",
+      red500: "#DE4D4D",
+      red600: "#D93333",
+      red700: "#D51A1A",
+      red800: "#D00000",
+      red900: "#BB0000",
+
+      blue000: "#EBF1FF",
+      blue100: "#C2D6FF",
+      blue200: "#99BBFF",
+      blue300: "#70A0FF",
+      blue400: "#4785FF",
+      blue500: "#1F69FF",
+      blue600: "#0052F5",
+      blue700: "#004FEB",
+      blue800: "#0044CC",
+      blue900: "#003DB8",
+
+      white: "#ffffff",
+    },
+    space: {},
+    fontSizes: {},
+    fonts: {},
+    fontWeights: {},
+    lineHeights: {},
+    letterSpacings: {},
+    sizes: {},
+    borderWidths: {},
+    borderStyles: {},
+    radii: {},
+    shadows: {},
+    zIndices: {},
+    transitions: {},
+  },
+});
