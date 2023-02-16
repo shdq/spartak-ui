867e1975dbfdcc7d1898ec64ef2b719dea8dbeb7
Sergei Cherniaev <sergei@cherniaev.com>
2023-02-16 15:24:17 +0400

    test(TextInput): add base tests for text input

--- components/input/TextInput.test.tsx
+++ components/input/TextInput.test.tsx
@@ -10,4 +10,38 @@ describe("TextInput", () => {
     // Act & Assert
     expect(input).toBeInTheDocument();
   });
+
+  test("should have placeholder", () => {
+    // Arrange
+    render(<TextInput placeholder="My placeholder" />);
+    const input = screen.getByRole("textbox");
+
+    // Act & Assert
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("placeholder", "My placeholder");
+  });
+
+  test("should be focusable", () => {
+    // Arrange
+    render(<TextInput />);
+    const input = screen.getByRole("textbox");
+
+    // Act & Assert
+    input.focus();
+    expect(input).toHaveFocus();
+
+    input.blur();
+    expect(input).not.toHaveFocus();
+  });
+
+  test("should be disabled", () => {
+    // Arrange
+    render(<TextInput disabled />);
+
+    // Act
+    const input = screen.getByRole("textbox");
+
+    // Assert
+    expect(input).toBeDisabled();
+  });
 });
