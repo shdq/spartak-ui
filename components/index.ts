4896a7e2b7826825ee0b66c5892b69a41aaa7bb2
Sergei Cherniaev <sergei@cherniaev.com>
2023-03-11 16:41:03 +0400

    refactor(text-input): rename TextInput directory

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,6 @@ export * from "./button";
 export * from "./card";
 export * from "./code";
 export * from "./heading";
-export * from "./input";
 export * from "./provider";
 export * from "./text";
+export * from "./text-input";
