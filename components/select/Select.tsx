8061b4cfa034829aba6046d197cc40d00fa7b1a9
Sergei Cherniaev <sergei@cherniaev.com>
2024-09-27 13:10:24 +0400

    feat(select): add select component, docs, stories

---
+++ components/select/Select.tsx
@@ -0,0 +1,341 @@
+import { useId } from "react";
+import { styled } from "../stitches.config";
+
+const DefaultIconContainer = styled("svg", {
+  position: "relative",
+  top: "5%",
+
+  variants: {
+    disabled: {
+      true: {
+        opacity: 0.6,
+        cursor: "not-allowed",
+      },
+      false: {
+        opacity: 1,
+        cursor: "pointer",
+      },
+    },
+  },
+  defaultVariants: {
+    disabled: false,
+  },
+});
+
+const SelectWrapper = styled("div", {
+  position: "relative",
+  fontFamily: "$system",
+  fontWeight: "$normal",
+  textAlign: "left",
+  color: "$grey700",
+  variants: {
+    size: {
+      xs: {
+        fontSize: "$xs",
+      },
+      sm: {
+        fontSize: "$sm",
+      },
+      md: {
+        fontSize: "$md",
+      },
+      lg: {
+        fontSize: "$lg",
+      },
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
+
+const SelectComponent = styled("select", {
+  all: "unset",
+  cursor: "pointer",
+  border: "$borderWidths$1 solid transparent",
+  borderRadius: "$3",
+  fontFamily: "$system",
+  fontWeight: "$normal",
+  textAlign: "left",
+  color: "$grey700",
+  width: "100%",
+  boxSizing: "border-box",
+  appearance: "none",
+  userSelect: "none",
+
+  "&:disabled": {
+    cursor: "not-allowed",
+    opacity: 0.6,
+  },
+  "&:focus-visible": {
+    borderColor: "$focus",
+  },
+
+  variants: {
+    variant: {
+      filled: {
+        backgroundColor: "$grey000",
+      },
+      outlined: {
+        backgroundColor: "transparent",
+        borderColor: "$grey400",
+        "&:focus-visible": {
+          borderColor: "$focus",
+        },
+      },
+    },
+    size: {
+      xs: {
+        fontSize: "$xs",
+        height: "$sizes$xs",
+        padding: "0 $paddingXS",
+      },
+      sm: {
+        fontSize: "$sm",
+        height: "$sizes$sm",
+        padding: "0 $paddingSM",
+      },
+      md: {
+        fontSize: "$md",
+        height: "$sizes$md",
+        padding: "0 $paddingMD",
+      },
+      lg: {
+        fontSize: "$lg",
+        height: "$sizes$lg",
+        padding: "0 $paddingLG",
+      },
+    },
+    isInvalid: {
+      true: {
+        borderColor: "$red500 !important",
+        "&:focus-visible": {
+          borderColor: "$focus !important",
+        },
+      },
+    },
+    withIcon: {
+      true: {},
+    },
+  },
+  compoundVariants: [
+    {
+      withIcon: true,
+      size: "xs",
+      css: {
+        paddingLeft: "$sizes$xs",
+      },
+    },
+    {
+      withIcon: true,
+      size: "sm",
+      css: {
+        paddingLeft: "$sizes$sm",
+      },
+    },
+    {
+      withIcon: true,
+      size: "md",
+      css: {
+        paddingLeft: "$sizes$md",
+      },
+    },
+    {
+      withIcon: true,
+      size: "lg",
+      css: {
+        paddingLeft: "$sizes$lg",
+      },
+    },
+  ],
+  defaultVariants: {
+    variant: "filled",
+    size: "sm",
+  },
+});
+
+const Label = styled("label", {
+  display: "inline-block",
+  marginBottom: "3px",
+  userSelect: "none",
+  variants: {
+    disabled: {
+      true: {
+        cursor: "not-allowed",
+        opacity: 0.6,
+      },
+    },
+  },
+});
+
+const SupportingText = styled("span", {
+  variants: {
+    variant: {
+      error: {
+        color: "$red500",
+      },
+      description: {
+        color: "$grey500",
+      },
+    },
+    size: {
+      xs: {
+        fontSize: "$xxs",
+      },
+      sm: {
+        fontSize: "$xs",
+      },
+      md: {
+        fontSize: "$sm",
+      },
+      lg: {
+        fontSize: "$md",
+      },
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
+
+const AsteriskContainer = styled("span", {
+  color: "$red500",
+  userSelect: "none",
+});
+
+const Asterisk = (): JSX.Element => {
+  return <AsteriskContainer>&nbsp;*</AsteriskContainer>;
+};
+
+const IconWrapper = styled("div", {
+  position: "absolute",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  zIndex: "1",
+  color: "$grey500",
+  pointerEvents: "none",
+
+  variants: {
+    position: {
+      start: {
+        left: 0,
+      },
+      end: {
+        right: 0,
+      },
+    },
+    size: {
+      xs: {
+        height: "$sizes$xs",
+        width: "$sizes$xs",
+      },
+      sm: {
+        height: "$sizes$sm",
+        width: "$sizes$sm",
+      },
+      md: {
+        height: "$sizes$md",
+        width: "$sizes$md",
+      },
+      lg: {
+        height: "$sizes$lg",
+        width: "$sizes$lg",
+      },
+    },
+  },
+  defaultVariants: {
+    position: "start",
+    size: "sm",
+  },
+});
+
+export interface SelectProps
+  extends React.ComponentProps<typeof SelectComponent> {
+  options: { value: string; label: string }[];
+  description?: string;
+  error?: string;
+  endIcon?: React.ReactNode;
+  icon?: React.ReactNode;
+  label?: string;
+  required?: boolean;
+}
+
+export const Select = ({
+  options,
+  description,
+  disabled,
+  error,
+  endIcon,
+  icon,
+  label,
+  required,
+  size,
+  ...props
+}: SelectProps): JSX.Element => {
+  const id = useId();
+  const isInvalid = error !== undefined;
+  const withIcon = icon !== undefined;
+
+  return (
+    <SelectWrapper size={size}>
+      {label && (
+        <Label htmlFor={id} disabled={disabled}>
+          {label}
+          {required && <Asterisk />}
+        </Label>
+      )}
+      {withIcon && (
+        <IconWrapper position="start" size={size}>
+          {icon}
+        </IconWrapper>
+      )}
+      {!endIcon && ( // when end icon isn't specified, we use defaul icon
+        <IconWrapper position="end" size={size}>
+          <DefaultIconContainer
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none" // Change this to 'none' for line-based stroke
+            stroke="currentColor" // Use stroke instead of fill
+            strokeWidth="1.2" // Adjust stroke width to make the lines thinner
+            strokeLinecap="round" // Makes the line ends rounded
+            strokeLinejoin="round" // Makes the corners smooth
+            disabled={disabled}
+          >
+            <path
+              d="M7.41 8.59L12 13.17l4.59-4.58" // Caret down path with stroke
+            />
+          </DefaultIconContainer>
+        </IconWrapper>
+      )}
+      {endIcon && (
+        <IconWrapper position="end" size={size}>
+          {endIcon}
+        </IconWrapper>
+      )}
+      <SelectComponent
+        disabled={disabled}
+        withIcon={withIcon}
+        isInvalid={isInvalid}
+        aria-invalid={isInvalid}
+        id={id}
+        size={size}
+        {...props}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </SelectComponent>
+      {(description || isInvalid) && (
+        <SupportingText
+          variant={isInvalid ? "error" : "description"}
+          size={size}
+        >
+          {isInvalid ? error : description}
+        </SupportingText>
+      )}
+    </SelectWrapper>
+  );
+};
